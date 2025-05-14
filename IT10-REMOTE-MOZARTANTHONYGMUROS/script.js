6b63aa8523c155b8d93b575535a1b8f3838345d8
alt-x3ys <murosmozartanthony@gmail.com>
2025-05-14 10:02:16 +0800

    Added JavaScript to handle form submit and assign correct name variable

---
+++ IT10-REMOTE-MOZARTANTHONYGMUROS/script.js
@@ -0,0 +1,8 @@
+const nameForm = document.getElementById("nameForm");
+const submitBtn = document.getElementById("submitbtn");
+const nameInput = document.getElementById("name");
+const myName = "Mozart";
+
+nameForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+})
