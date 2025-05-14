62029c2d497685a0a5723032d86898686b1092e2
alt-x3ys <murosmozartanthony@gmail.com>
2025-05-14 10:35:28 +0800

    Added success message and green border for valid input

--- IT10-REMOTE-MOZARTANTHONYGMUROS/script.js
+++ IT10-REMOTE-MOZARTANTHONYGMUROS/script.js
@@ -21,5 +21,8 @@ document.getElementById("submitbtn").addEventListener('click', function(){
     if (nameValue === myName){
         document.getElementById("name").style.borderColor = "green";
         alert('Correct name.');
+    } else {
+        document.getElementById("name").style.borderColor = "red";
+        alert('Incorrect name.');
     }
 });
