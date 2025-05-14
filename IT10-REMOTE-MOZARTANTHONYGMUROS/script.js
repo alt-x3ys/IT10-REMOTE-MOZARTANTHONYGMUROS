6dfc717041ce319cef9cf299b4f918bc4044b9f4
alt-x3ys <murosmozartanthony@gmail.com>
2025-05-14 10:26:37 +0800

    Added alert when input field is empty

--- IT10-REMOTE-MOZARTANTHONYGMUROS/script.js
+++ IT10-REMOTE-MOZARTANTHONYGMUROS/script.js
@@ -1,8 +1,14 @@
-const nameForm = document.getElementById("nameForm");
-const submitBtn = document.getElementById("submitbtn");
-const nameInput = document.getElementById("name");
+const nameForm = document.querySelector("#nameForm");
+const submitBtn = document.querySelector("#submitbtn");
 const myName = "Mozart";
 
-nameForm.addEventListener('submit', function(event) {
+
+
+
+nameForm.addEventListener('submit', function(event){
     event.preventDefault();
 })
+
+document.getElementById("name").addEventListener('blur', function(){
+    alert("Input field is empty.")
+});
